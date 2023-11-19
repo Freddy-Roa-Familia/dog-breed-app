72563a24fbcaea4e7f9a2d0be879f3f46cc72093
Freddy Roa-Familia <freddy-roa-familia@FRs-MacBook-Air.local>
2023-11-18 21:19:03 -0400

    Created a Dog component and added it to the main view.

---
+++ src/app/dog/page.tsx
@@ -0,0 +1,17 @@
+
+export default function Dog (props:any) {
+  let breed = props.breed;
+  let image = props.image;
+  
+  return (
+    <>
+      <div className="flex flex-col items-center">
+        <img 
+          className='rounded-full h-52 w-52' 
+          src={image} 
+          alt={breed}></img>
+        <p className='text-justify'>{breed}</p>
+      </div>
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Dog from './dog/page';
 
 export default function Home() {
   return (
     <>
-      <h1> Under Construction </h1>
+      <div className='grid grid-cols-2 gap-4'>
+        <div><Dog image='breeds/germanshepherd.png' breed='German Shepherd' /></div>
+        <div><Dog image='breeds/frenchbulldog.png' breed='French Bulldog' /></div>
+        <div><Dog image='breeds/canecorso.png' breed='Cane Corso' /></div>
+        <div><Dog image='breeds/goldenretriever.png' breed='Golden Retriever' /></div>
+      </div>
     </>
   )
 }
