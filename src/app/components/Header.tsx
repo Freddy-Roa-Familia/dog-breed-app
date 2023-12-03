b4603aee3cdaf38d790f69b539f0f36c0f8c4f0b
Freddy Roa-Familia <freddyroafamilia@gmail.com>
2023-12-03 18:14:55 -0500

    Added an interface to the Header component.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,23 @@
-export default function Header(props:any) {
+interface HeaderProps {
+  title: string;
+  logo: string;
+}
+
+export default function Header(props: HeaderProps) {
   let title = props.title;
   let logo = props.logo;
 
   return (
     <>
-      <div className="flex items-center">
+      <div className="flex items-center ml-4">
         <h1 className="text-3xl"> {title} </h1>
-        <img 
-          src={logo} 
+        <img
+          src={logo}
           alt="Default Logo"
           className="w-44"
         >
         </img>
       </div>
-      
     </>
   )
 }
