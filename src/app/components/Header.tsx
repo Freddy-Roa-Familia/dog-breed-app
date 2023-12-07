68818fa9165d6e5acf2018cd90623701cd67b407
Freddy Roa-Familia <freddyroafamilia@gmail.com>
2023-12-07 16:39:44 -0500

    Destrutured the props in the Header component.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,10 +3,8 @@ interface HeaderProps {
   logo: string;
 }
 
-export default function Header(props: HeaderProps) {
-  let title = props.title;
-  let logo = props.logo;
-
+export default function Header({ title, logo }: HeaderProps) {
+  
   return (
     <>
       <div className="flex items-center bg-indigo-400 pl-4 mb-4">
